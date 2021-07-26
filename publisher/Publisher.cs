bb10d99d7fd315f98ee7ab0ebed8728f37cb2b7c
Ali <malicicek1@gmail.com>
2021-07-27 01:01:28 +0300

    initial

---
+++ publisher/Publisher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using static System.Console;
+
+namespace publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://unpwxnla:4_fXpw3ZsSWd8xiDCK_t_J6lPvRtCNBE@elk.rmq2.cloudamqp.com/unpwxnla");
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("hello-queue", true, false, false);
+
+                Enumerable.Range(1, 50).ToList().ForEach(a =>
+                {
+                    string msg = a + ". Hello World!";
+
+                    var body = Encoding.UTF8.GetBytes(msg);
+
+                    channel.BasicPublish(String.Empty, "hello-queue", null, body);
+
+                    WriteLine("Message has been sent. Message : " + msg);
+
+                });
+
+
+                ReadLine();
+            }
+
+        }
+    }
+}
---
+++ subscriber/Subscriber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using static System.Console;
+
+namespace subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://unpwxnla:4_fXpw3ZsSWd8xiDCK_t_J6lPvRtCNBE@elk.rmq2.cloudamqp.com/unpwxnla");
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //channel.QueueDeclare("hello-queue", true, false, false);
+
+                channel.BasicQos(0, 1, false);
+                var consumer = new EventingBasicConsumer(channel);
+
+                channel.BasicConsume("hello-queue", false, consumer);
+
+                consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+                {
+                    var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                    Thread.Sleep(1500);
+                    WriteLine("Receiving message :" + msg);
+
+                    channel.BasicAck(e.DeliveryTag, false);
+                };
+
+            }
+            ReadLine();
+        }
+    }
+}
